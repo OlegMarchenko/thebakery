b3bc9a4cd92a48a0c5ded0f526ad6dc3d82992c5
OlegMarchenko <workolegmarchenko@gmail.com>
2017-10-04 22:51:01 +0300

    add color-scheme

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $('.js-theme-dark').on('click', function() {
+        $('.header-container').removeClass('light-header').addClass('dark-header');
+        $(this).addClass('active');
+        $('.js-theme-light').removeClass('active');
+    });
+
+    $('.js-theme-light').on('click', function() {
+        $('.header-container').addClass('light-header').removeClass('dark-header');
+        $(this).addClass('active');
+        $('.js-theme-dark').removeClass('active');
+    });
+
+});
