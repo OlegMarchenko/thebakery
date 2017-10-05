843c96822b42943e6b788453a10e28e9c5010826
OlegMarchenko <workolegmarchenko@gmail.com>
2017-10-05 23:01:46 +0300

    add dark-scheme

--- js/common.js
+++ js/common.js
@@ -2,12 +2,20 @@ $(document).ready(function () {
 
     $('.js-theme-dark').on('click', function() {
         $('.header-container').removeClass('light-header').addClass('dark-header');
+        $('.section-about-us').addClass('dark-about-us');
+        $('.section-our-products').addClass('section-our-products-dark');
+        $('.pre-footer').removeClass('section-contact-us').addClass('section-contact-us-dark');
+        $('.footer').removeClass('footer-light').addClass('footer-dark');
         $(this).addClass('active');
         $('.js-theme-light').removeClass('active');
     });
 
     $('.js-theme-light').on('click', function() {
         $('.header-container').addClass('light-header').removeClass('dark-header');
+        $('.section-about-us').removeClass('dark-about-us');
+        $('.section-our-products').removeClass('section-our-products-dark');
+        $('.pre-footer').removeClass('section-contact-us-dark').addClass('section-contact-us');
+        $('.footer').removeClass('footer-dark').addClass('footer-light');
         $(this).addClass('active');
         $('.js-theme-dark').removeClass('active');
     });
